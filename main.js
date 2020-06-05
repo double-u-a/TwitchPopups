48a0f20f924239c526dab5a2047e111d0839b5b8
wa <login@double-u-a>
2020-06-05 14:22:17 +0100

    consolidate

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// main
+
 const opts = {channels: [twitchChannel],connection: {secure: true,reconnect: true}};
 
 let actionHandlers = {};
@@ -35,3 +37,98 @@ function onMessageHandler(target, context, msg, self) {
 }
 
 function onConnectedHandler(addr, port) {console.log(`* Connected to ${addr}:${port}`);}
+
+
+
+// popup
+
+const popup = {
+    /* Displays popup on screen with the given text, font and colour. */
+    showText: (text, bgColour, textColour, textFont) => {
+
+        document.documentElement.style.setProperty('--bgColour', bgColour);
+        document.documentElement.style.setProperty('--textColour', textColour);
+        document.documentElement.style.setProperty('--textFont', textFont);
+
+        document.getElementById('popupbox').display = '';
+        document.getElementById('popuptext').innerHTML = text;
+        document.getElementById("popupbox").animate([{opacity: 0},{opacity:1}],{ duration: 700, easing: 'ease-in-out', fill: 'forwards'});   
+
+        const textWidth = parseFloat(getComputedStyle(document.getElementById('popuptext'), null).width.replace("px", ""))
+        document.getElementById('popupbox').style.setProperty('width', textWidth + 30 + 'px');
+
+    },
+
+    /* Removes popup from screen */
+    delete: () => {
+        spotlightUser = ""; // TODO: Remove this
+        document.getElementById("popupbox").animate([{opacity: 1},{opacity:0}],{ duration: 700, easing: 'ease-in-out', fill: 'forwards'});   
+    },
+    
+    /* Formats text */
+    formatMessage: (message, makeUpperCase) => {
+
+        // uppercase text
+        if (makeUpperCase) {
+            message = message.toUpperCase();
+        }
+
+        return message;
+    }
+}
+
+
+
+// Handlers
+
+// Command: #alert <text>
+// Description: will display whatever text comes after the !alert command
+actionHandlers['#alert'] = {
+    security: (context, textContent) => {
+        return context.mod || (context["badges-raw"] != null && context["badges-raw"].startsWith("broadcaster"))
+    },
+    handle: (context, textContent) => {
+        const formattedText = popup.formatMessage(textContent, makeAlertUpperCase).substr(7);
+        popup.showText(formattedText, alertBg, alertTextColour, textFont);
+    }
+};
+
+
+// Command: ###
+// Description: This delete command resets the whole pop up system
+actionHandlers['###'] = {
+    security: (context, textContent) => {
+        return context.mod || (context["badges-raw"] != null && context["badges-raw"].startsWith("broadcaster"))
+    },
+    handle: (context, textContent) => {
+        popup.delete();
+        // TODO : loop through objects calling its own state reset function
+    }
+};
+
+
+// Command: #spot
+// Description: spotlight [@username]: will display the chat of the specified user from that point on
+var spotlightUser = "";
+
+actionHandlers['#spot'] = {
+    security: (context, textContent) => {
+        return context.mod || (context["badges-raw"] != null && context["badges-raw"].startsWith("broadcaster"))
+    },
+    handle: (context, textContent) => {
+        spotlightUser = textContent.substr(12).toLowerCase();
+        popup.showText(`${spotlightEmoji} Welcome ${spotlightUser} to the stage!`, spotlightBg, spotlightTextColour,textFont);
+    }
+};
+
+// This handler is fired when the spotlighted user types something in chat
+allHandlers.push({
+    security: (context, textContent) => {
+        return context.username === spotlightUser && (!textContent.startsWith('@') || textContent.startsWith('@' + twitchChannel))
+    },
+    handle: (context, textContent) => {
+        const formattedText = popup.formatMessage(textContent);
+        console.log(formattedText);
+        popup.showText(`${spotlightEmoji} ${context['display-name']}: ${formattedText}`, spotlightBg, spotlightTextColour,textFont);
+    }
+});
